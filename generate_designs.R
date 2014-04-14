b1faf7201afbb27a4acaf5885fbbf16671ef6775
tylerascott <tscott1@uw.edu>
2014-04-14 10:28:51 -0700

    generate designs update
    script used to play around with designs

--- generate_designs.R
+++ generate_designs.R
@@ -92,20 +92,16 @@ stopCluster(cl)
 
 save.image('designs_24block.RData')
 
-#9 (gather resources) and 10 (able to harvest)
-#3 (identity) and 5 (inspiration)
-#2 (attached) and 5 (inspiration)
-#2 (attached) and 3 (identity)
-#14 (trust policymakers) and 15 (trust experts)
-#12 (environmental stewardship) and/or 14, 15
 
+
+tempAA<-unlist(tempA,recursive=F)
 
 #generate 16 fake datasets based upon 16 designs
-temp1<-lapply(1:length(tempA), function(x) fakesample(design=tempA[[x]],data=sd,numinblock=12))
+temp1<-lapply(1:length(tempAA), function(x) fakesample(design=tempAA[[x]],data=sd,numinblock=150))
 
 #make mi.info objects for each fake dataset
 temp2 <- llply(.data=temp1, .fun= mi.info) # .parallel=TRUE, .paropts= list(.packages=c('mi')))
-temp2a <- llply(.data=temp1a, .fun= mi.info) # .parallel=TRUE, .paropts= list(.packages=c('mi')))
+
 
 #change colnames of fake data to 'X1' etc. 
 for (i in 1:length(temp1))
@@ -113,11 +109,6 @@ for (i in 1:length(temp1))
   colnames(temp1[[i]])<-paste0('X',seq(1,15,1))
 }
 
-#change colnames of fake data to 'X1' etc. 
-for (i in 1:length(temp1a))
-{
-  colnames(temp1a[[i]])<-paste0('X',seq(1,15,1))
-}
 
 
 require(doParallel)
@@ -127,61 +118,107 @@ multimputesA<-
   llply(.data=temp1, .fun=mi,
         n.iter=20,check.coef.convergence=TRUE,add.noise=FALSE,max.minutes=8,
         .parallel=TRUE,.paropts = list(.packages='mi',.verbose=TRUE),.progress='tk')
+
 stopCluster(cl)
-save.image('runmiresults36.RData')
-
-require(doParallel)
-cl<-makeCluster(16)
-registerDoParallel(cl)
-multimputesB<-
-  llply(.data=temp1a, .fun=mi,
-        n.iter=20,check.coef.convergence=TRUE,add.noise=FALSE,max.minutes=8,
-        .parallel=TRUE,.paropts = list(.packages='mi',.verbose=TRUE),.progress='tk')
-stopCluster(cl)
-save.image('runmiresults24.RData')
-
-
-
 da<-llply(.data=multimputesA, .fun=mi.completed,.progress='text')
-dab<-llply(.data=multimputesB, .fun=mi.completed,.progress='text')
-
-
-
-
-#make each imputed dataset a data frame, select one of three imputed sets randomly for each imputation run
+#make each imputed dataset a data frame
 da1<-lapply(1:length(da),function(x) as.data.frame(da[[x]][1]))
-da1b<-lapply(1:length(dab),function(x) as.data.frame(dab[[x]][1]))
 #make values numeric
-
 da2<-lapply(1:length(da1),function(x) apply(da1[[x]],2,as.numeric))
-da2b<-lapply(1:length(da1b),function(x) apply(da1b[[x]],2,as.numeric))
-
 numdat<-apply(sd,2,as.numeric)
-
 fakecor<-llply(.data=da2,.fun=cor,use='pairwise.complete.obs',method='spearman',.progress='text')
-fakecorb<-llply(.data=da2b,.fun=cor,use='pairwise.complete.obs',method='spearman',.progress='text')
-
 obscor<-cor(numdat,use='pairwise.complete.obs',method='spearman')
-
 #compar faked to observed (toss out values where NA for one question)
 cordiff.score<-lapply(1:length(fakecor),function(x) sum(abs(obscor-fakecor[[x]])))
-cordiff.score.B<-lapply(1:length(fakecor),function(x) sum(abs(obscor-fakecorb[[x]])))
-save.image('searchfordesign.3624blocks.Sim3.RData')
+save.image('searchfordesign.24blocks.Sim3.RData')
 
 
-cordiff.score
+cor(numdat,use='pairwise.complete.obs',method='spearman')
 
 min(unlist(cordiff.score))
-sum(unlist(cordiff.score)<16.0)
+
+sum(unlist(cordiff.score)<16.5)
 
 
 
 best <- which.min(unlist(cordiff.score))
-small3 <- which(unlist(cordiff.score)<16.0)
+small3 <- which(unlist(cordiff.score)<16.5)
 
 #best
-tabulate(tempA[[best]])
-bd<-tempA[[best]]
+apply(tempAA[[best]],1,sort)
+#9 (gather resources) and 10 (able to harvest) #2
+#3 (identity) and 5 (inspiration) #2
+#2 (attached) and 5 (inspiration) #
+#2 (attached) and 3 (identity) #2
+#14 (trust policymakers) and 15 (trust experts) #2
+#12 (environmental stewardship) and/or 14, 15  #12/14 = 3, #12/15 = 1
+
+emp<-as.vector(NULL)
+pp<-t(apply(bd,1,sort))
+for (u in 1:nrow(pp))
+{for (v in 1:ncol(pp)){if (v != ncol(pp))
+{emp<-append(emp,(paste(pp[u,v],pp[u,(v+1):ncol(pp)])))}}}
+sort(table(emp))
+
+
+2 5  3 12  3 14  7 10  8 10  8 15
+
+
+
+for (i in 1:nrow(bd))
+{
+  for( j in 1:ncol(bd))
+  {
+    (tc[bd[i,j],bd[i,]])
+  }
+}
+
+
+
+cor(numdat,use='pairwise.complete.obs',method='spearman')
+
+tc<-cor(numdat,use='pairwise.complete.obs',method='spearman')
+all(
+  paste(t(combn(15,2))[,1],t(combn(15,2))[,2])
+  
+  %in% emp)
+
+tabulate(tempAA[[best]])
+testswitch<-tempAA[[best]]
+testswitch[1,5]<-5
+
+cordiff.score[[small3[2]]]
+
+apply(tempAA[[small3[2]]],1,sort)
+#9 (gather resources) and 10 (able to harvest) #9/10 = 2
+#3 (identity) and 5 (inspiration) #3/5 = 2
+#2 (attached) and 5 (inspiration) #2/5 = 4
+#2 (attached) and 3 (identity) #2/3 = 3
+#14 (trust policymakers) and 15 (trust experts) #14/15 = 3
+#12 (environmental stewardship) and/or 14, 15  #12/14 = 1, #12/15 = 1, #14/15 = 3
+
+
+apply(tempAA[[small3[1]]],1,sort)
+#9 (gather resources) and 10 (able to harvest) #9/10 = 2
+#3 (identity) and 5 (inspiration) #3/5 = 1
+#2 (attached) and 5 (inspiration) #2/5 = 2
+#2 (attached) and 3 (identity) #2/3 = 2
+#14 (trust policymakers) and 15 (trust experts) #14/15 = 2
+#12 (environmental stewardship) and/or 14, 15  #12/14 = 1, #12/15 = 2, #14/15 = 2
+
+mm<-apply(sd,2,as.numeric)
+nn<-as.matrix(mm)
+nn
+cor(nn,na.rm=TRUE)
+?cor
+
+cormatrix(sd)
+as.numeric(sd)
+cor(as.numeric(sd))
+
+
+tabulate(tempAA[best]])
+bd<-tempAA[[small3[2]]]
 qlabels
 bdq<-bd
 for (i in 1:nrow(bd))
